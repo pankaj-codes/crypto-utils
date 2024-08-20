30d431fda54aa75ec029d591adeed9cdbff6c3d8
Pankaj Yadav <pyadav@cloudbees.com>
2024-08-20 20:57:46 +0530

    Encrypt and decrypt plain text and cipher text using RSA

--- src/main/java/com/pankaj/crypto/asymmetric/AsymmetricEncryptionUtils.java
+++ src/main/java/com/pankaj/crypto/asymmetric/AsymmetricEncryptionUtils.java
@@ -2,7 +2,10 @@ package com.pankaj.crypto.asymmetric;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
+import javax.crypto.Cipher;
 
 public class AsymmetricEncryptionUtils {
     private static final String RSA = "RSA";
@@ -14,5 +17,18 @@ public class AsymmetricEncryptionUtils {
         // To seed it secure random is used
         keyPairGenerator.initialize(4096, secureRandom);
         return keyPairGenerator.generateKeyPair();
+    }
+
+    public static byte[] performRSAEncryption(String plainText, PrivateKey privateKey) throws Exception{
+        Cipher cipher = Cipher.getInstance(RSA);
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+        return cipher.doFinal(plainText.getBytes());
+    }
+
+    public static String performRSADecryption(byte[] cipherText, PublicKey publicKey) throws Exception{
+        Cipher cipher = Cipher.getInstance(RSA);
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        byte[] decryptedText = cipher.doFinal(cipherText);
+        return new String(decryptedText);
     }
 }
--- src/test/java/com/pankaj/crypto/asymmetric/AsymmetricEncryptionUtilsTest.java
+++ src/test/java/com/pankaj/crypto/asymmetric/AsymmetricEncryptionUtilsTest.java
@@ -17,4 +17,15 @@ class AsymmetricEncryptionUtilsTest {
         System.out.println("Private Key :" + DatatypeConverter.printHexBinary(keyPair.getPrivate().getEncoded()));
         System.out.println("Public Key :" + DatatypeConverter.printHexBinary(keyPair.getPublic().getEncoded()));
     }
+
+    @Test
+    void testRSACryptoRoutine() throws Exception{
+        KeyPair keyPair = AsymmetricEncryptionUtils.generateRSAKeyPair();
+        String plainText = "This is going to hide in plain sight";
+        byte[] cipherText = AsymmetricEncryptionUtils.performRSAEncryption(plainText, keyPair.getPrivate());
+        assertNotNull(cipherText);
+        System.out.println(DatatypeConverter.printHexBinary(cipherText));
+        String decryptedText = AsymmetricEncryptionUtils.performRSADecryption(cipherText, keyPair.getPublic());
+        assertEquals(plainText, decryptedText);
+    }
 }
